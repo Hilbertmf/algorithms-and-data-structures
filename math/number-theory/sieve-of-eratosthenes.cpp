94f68b57085cf0642b8bb4e7f83f989fb1e02b78
hilbert <hilbertmf@gmail.com>
2022-07-02 22:49:27 -0300

    add sieve of eratosthenes

---
+++ math/number-theory/sieve-of-eratosthenes.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> getPrimes(int n) {
+    vector<bool> isPrime(n+1, true);
+    vector<int> primes;
+    isPrime[0] = false;
+    isPrime[1] = false;
+    
+    for(int i = 2; i*i <= n; ++i) {
+        if(!isPrime[i]) continue;
+        
+        for(int j = i*i; j <= n; j += i) {
+            isPrime[j] = false;
+        }
+    }
+
+    for(int i = 0; i <= n; ++i) {
+        if(isPrime[i]) primes.push_back(i);
+    }
+    
+    return primes;
+}
