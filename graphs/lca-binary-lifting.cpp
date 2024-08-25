d5f6d3e19249110cc931f80086b78d389e3bec27
hilbert-franca <hilbertmf@gmail.com>
2024-08-25 00:35:20 -0300

    update lca and binary lifting

--- graphs/lca-binary-lifting.cpp
+++ graphs/lca-binary-lifting.cpp
@@ -2,59 +2,41 @@
 #define DEBUG(x) cout << #x << " >>>> " << x << endl 
 using namespace std;
 
-int dfs(int root, vector<vector<int>>& tree, int d,
-vector<int>& depth) {
-    depth[root] = d;
-    int curr = d;
-    for(auto &adj : tree[root]) {
-        curr = max(curr, dfs(adj, tree, d + 1, depth));
+void getDepth(int node, int parent,
+vector<int>& par, vector<vector<int>>& tree,
+vector<int>& depth, int d) {
+    par[node] = parent;
+    depth[node] = d;
+    for(auto &adj : tree[node]) {
+        if (adj == parent) continue;
+        getDepth(adj, node, par, tree, depth, d + 1);
     }
-    return curr;
-}
-
-int getDepth(int root, vector<int>& par, vector<int>& depth) {
-    int n = par.size();
-    vector<vector<int>> tree(n);
-    for(int i = 1; i < n; ++i) {
-        tree[par[i]].push_back(i);
-    }
-    return dfs(root, tree, 0, depth);
 }
 
 int getLOG(int depth) {
-    int LOG = 0;
-    while ((1 << LOG) <= depth) {
-        LOG++;
-    }
+    int LOG = 1;
+    while ((1 << LOG) <= depth) LOG++;
     return LOG;
 }
 
-
 int goUp(int& node, int steps, vector<vector<int>>& up, int LOG) {
-    for(int i = LOG - 1; i >= 0; --i) {
-        // if we can make that jump
-        if ((1 << i) <= steps) {
-            steps -= (1 << i);
+    for(int i = 0; i < LOG; ++i, steps >>= 1) {
+        if (steps & 1)
             node = up[node][i];
-        }
     }
     return node;
 }
 
-int lca(int a, int b, vector<vector<int>>& up, int LOG,
-vector<int>& depth) {
-    if (a == 0 || b == 0) return 0;
-
-    // make depth equal:
+int lca(int a, int b, vector<vector<int>>& up, int LOG, vector<int>& depth) {
     if (depth[a] < depth[b])
         swap(a, b);
 
     int diff_depth = depth[a] - depth[b];
     goUp(a, diff_depth, up, LOG);
-    
+
     if (a == b) return a;
 
-    for(int i = LOG - 1; i >= 0; --i) {
+    for (int i = LOG - 1; i >= 0; --i) {
         if (up[a][i] != up[b][i]) {
             a = up[a][i];
             b = up[b][i];
@@ -64,19 +46,20 @@ vector<int>& depth) {
     return up[a][0];
 }
 
-void binaryLifting(vector<int>& par, int& LOG,
-vector<int>& depth, vector<vector<int>>& up) {
-
+void binaryLifting(vector<int>& par, int& LOG, vector<int>& depth,
+vector<vector<int>>& up, vector<vector<int>>& tree) {
     int n = par.size();
-    int d = getDepth(0, par, depth);
-    LOG = getLOG(d);
-    up = vector<vector<int>> (n, vector<int>(LOG));
+    // choose 0 as the root
+    int root = 0;
+    getDepth(root, root, par, tree, depth, 0);
+    LOG = getLOG(n);
+    up = vector<vector<int>>(n, vector<int>(LOG));
 
-    // first set all immediate parents
-    for(int i = 0; i < n; ++i)
+    // set parents
+    for (int i = 0; i < n; ++i)
         up[i][0] = par[i];
-    for(int j = 1; j < LOG; ++j) {
-        for(int i = 1; i < n; ++i) {
+    for (int j = 1; j < LOG; ++j) {
+        for (int i = 1; i < n; ++i) {
             up[i][j] = up[ up[i][j - 1] ][j - 1];
         }
     }
@@ -87,18 +70,32 @@ int main()  {
     int n, q;
     cin >> n >> q;
     vector<int> par(n);
+    vector<vector<int>> tree(n);
+
+    // if 0 is root:
     par[0] = 0;
     for(int i = 1; i < n; ++i) {
         int boss;
         cin >> boss;
         boss--;
         par[i] = boss;
+        tree[i].push_back(boss);
+        tree[boss].push_back(i);
     }
+
+    // if we receive the edges:
+    // for(int i = 0; i < n; ++i) {
+    //     int a, b;
+    //     cin >> a >> b;
+    //     --a; --b;
+    //     tree[a].push_back(b);
+    //     tree[b].push_back(a);
+    // }
 
     vector<int> depth(n);
     vector<vector<int>> up;
     int LOG;
-    binaryLifting(par, LOG, depth, up);
+    binaryLifting(par, LOG, depth, up, tree);
 
     while (q--) {
         int a, b;
